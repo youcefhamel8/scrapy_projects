b92b13b7bb1041b834f634d13e0ab2ed63b2c36b
youcefhamel8 <youcef.hamel8@gmail.com>
2020-12-20 00:33:41 +0100

    somm cleanin stuff
    This reverts commit 07c2e822d44d11c34e4991d19676f38fa0ff2de4.
    
    # Conflicts:
    #	.idea/workspace.xml

--- crunchbase/crunchbase/settings.py
+++ crunchbase/crunchbase/settings.py
@@ -23,12 +23,12 @@ NEWSPIDER_MODULE = 'crunchbase.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 6
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 2
+DOWNLOAD_DELAY = 60
 
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
